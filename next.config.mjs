135e20a1760fa58e81972e0db473f82e2ecdb312
jobeanghun <jobeanghun@gmail.com>
2026-04-21 14:44:01 +0900

    fix(deploy): forcefully trace all file-type dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  serverExternalPackages: ["pdf-parse"],
+  serverExternalPackages: [
+    "pdf-parse", 
+    "officeparser", 
+    "file-type", 
+    "strtok3", 
+    "token-types", 
+    "peek-readable", 
+    "ieee754"
+  ],
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,7 +17,15 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   outputFileTracingIncludes: {
-    '/api/**/*': ['./node_modules/pdf-parse/**/*'],
+    '/api/**/*': [
+      './node_modules/pdf-parse/**/*',
+      './node_modules/officeparser/**/*',
+      './node_modules/file-type/**/*',
+      './node_modules/strtok3/**/*',
+      './node_modules/token-types/**/*',
+      './node_modules/peek-readable/**/*',
+      './node_modules/ieee754/**/*'
+    ],
   },
 };
 
